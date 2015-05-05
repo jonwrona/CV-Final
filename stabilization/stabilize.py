253c6223b40c6b1fa3ba6184ac95c392e41f2b53
Jonathan Wrona <jonwrona@gmail.com>
2015-05-05 12:24:27 -0400

    added rough draft for image stabilization progress report.

--- stabilization/stabilize.py
+++ stabilization/stabilize.py
@@ -26,6 +26,7 @@ p0 = cv2.goodFeaturesToTrack(old_gray, mask = None, **feature_params)
 
 # Create a mask image for drawing purposes
 mask = np.zeros_like(old_frame)
+trans_list = []
 
 while(cap.grab()):
     ret, frame = cap.retrieve()
@@ -39,19 +40,10 @@ while(cap.grab()):
     good_old = p0[st==1]
 
     h, m = cv2.findHomography(good_old, good_new)
-    print h
 
     height, width = frame.shape[:2]
     img = cv2.warpPerspective(frame, h, (width, height))
     frame = img
-
-    # draw the tracks
-    # for i,(new,old) in enumerate(zip(good_new,good_old)):
-    #     a,b = new.ravel()
-    #     c,d = old.ravel()
-    #     cv2.line(mask, (a,b),(c,d), color[i].tolist(), 2)
-    #     cv2.circle(frame,(a,b),5,color[i].tolist(),-1)
-    # img = cv2.add(frame,mask)
 
     cv2.imshow('frame',img)
     k = cv2.waitKey(30) & 0xff
