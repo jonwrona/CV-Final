3264a8bed607397bca227a05316ad536fc56872f
danlowe95 <danlowe95@gmail.com>
2015-05-12 17:29:25 -0400

    added some debuggin text

--- blob-tracking/track_monkeys.py
+++ blob-tracking/track_monkeys.py
@@ -58,19 +58,29 @@ cv2.waitKey(0)
 # Create rectangles based on these points
 rectangles = []
 for pt in p0:
-	pt1 = (pt[0][0] - 36, pt[0][1] - 36)
-	rectangles.append((int(pt1[0]), int(pt1[1]), 72, 72))
+	pt1 = (pt[0][0] - 40 , pt[0][1] - 40)
+	rectangles.append((int(pt1[0]), int(pt1[1]), 80, 80))
 
 roi_list = []
+test_frame = copy.copy(old_frame)
 for rect in rectangles:
 	c, r, w, h = rect
 	roi = old_frame[r:r+h, c:c+w]
-	hsv_roi =  cv2.cvtColor(old_frame, cv2.COLOR_BGR2HSV)
+	hsv_roi =  cv2.cvtColor(roi, cv2.COLOR_BGR2HSV)
+	# These values (0, 60, 32), (180, 255, 255) are used in all of the examples. Why? I feel like changing them is the key to correctly tracking monkeys, but idk
+	# This creates a mask of values either 255 or 0 (1 or 0), based on if they are in the range provided.
 	mask = cv2.inRange(hsv_roi, np.array((0., 60.,32.)), np.array((180.,255.,255.)))
+	# We use 180 because HSV values only go up to 180
+	# We use the mask from above to do calcHist. Why are we making this histogram?
 	roi_hist = cv2.calcHist([hsv_roi],[0],mask,[180],[0,180])
+	# Normalize the histogram
 	cv2.normalize(roi_hist,roi_hist,0,255,cv2.NORM_MINMAX)
 	roi_list.append(roi_hist)
-
+	x,y,w,h = rect
+	cv2.rectangle(test_frame, (x,y), (x+w,y+h), 255,2)
+test_frame = cv2.resize(test_frame, (0,0), fx=.5, fy=.5)
+cv2.imshow('found boxes', test_frame)
+cv2.waitKey(0)
 term_crit = ( cv2.TERM_CRITERIA_EPS | cv2.TERM_CRITERIA_COUNT, 10, 1 )
 
 while(1):
@@ -79,6 +89,7 @@ while(1):
     if ret == True:
         hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
         for i, rect in enumerate(rectangles):
+        	# This is where the histograms come into play.  Need to look into calcBackProject to understand what it uses it for
         	dst = cv2.calcBackProject([hsv],[0],roi_list[i],[0,180],1)
 	        ret, track_window = cv2.meanShift(dst, rect, term_crit)
 	        rectangles[i] = track_window
@@ -88,7 +99,7 @@ while(1):
         imgshow = cv2.resize(frame, (0,0), fx=.5, fy=.5)
 
         cv2.imshow('frame', imgshow)
-        # time.sleep(1)
+        time.sleep(1)
         k = cv2.waitKey(60) & 0xff
         if k == 27:
             break
