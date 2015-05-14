72a7bc1225ccff2eeb26a6b69c9bfa35b21761c0
Avinash Singh <absingh696@gmail.com>
2015-05-14 12:15:37 -0400

    find centers also returns areas

--- background/bkgdsubtract.py
+++ background/bkgdsubtract.py
@@ -1,16 +1,20 @@
 import numpy as np
 import cv2
 import os
+import sys
 from matplotlib import pyplot as plt
 # import image_plot_utilities as ipl
 
-
 def create_frames( vidname ):
-	vid = cv2.VideoCapture('../../Gelada/'+vidname+'.mp4')
+	vid = cv2.VideoCapture('../../Gelada/'+vidname)
+	if not os.path.exists(vidname):
+		os.makedirs(vidname)
+		if not os.path.exists(vidname+'/samples'):
+			os.makedirs(vidname+'/samples');
 
 	mog2 = cv2.BackgroundSubtractorMOG2(200, 16, 0)
 
-	sampleidcs = [50,500,1000,1500]
+	sampleidcs = [50,100,300,600]
 
 	numframes = 1
 	while(1):
@@ -51,8 +55,12 @@ def create_frames( vidname ):
 
 def find_centers( vidname ):
 	frames = os.listdir(vidname+'/samples')
+	if not os.path.exists(vidname+'/detect'):
+		os.makedirs(vidname+'/detect')
+
 	kernel = cv2.getStructuringElement(cv2.MORPH_RECT,(3,3))
 	allcenters = []
+	allareas = []
 	for frame in frames:
 		im = cv2.imread(vidname+'/samples/'+frame)
 		im = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
@@ -64,19 +72,39 @@ def find_centers( vidname ):
 		imcpy = im.copy()
 		contours,hierarchy = cv2.findContours(imcpy,0,2)
 		framecenters = []
-		for contour in contours:
+		areas = []
+		contours = sorted(contours, key=cv2.contourArea, reverse=True)
+		topcontours = contours[:int(0.15*len(contours))]
+		for contour in topcontours:
 			M = cv2.moments(contour)
-		    # get coordinates of center of mass
-		    cx = int(M['m10']/M['m00'])
-		    cy = int(M['m01']/M['m00'])
-		    framecenters.append((cx, cy))
+			# get coordinates of center of mass
+			cx = int(M['m10']/M['m00'])
+			cy = int(M['m01']/M['m00'])
+			framecenters.append((cx, cy))
+
+			area = cv2.contourArea(contour)
+			areas.append(area)
 
 		allcenters.append(framecenters)
-		cv2.drawContours(im, [box], -1, (255,0,0))
-		cv2.imshow('sample1',im)
-		cv2.waitKey()
-		#cv2.imwrite('sample1.jpg',im)
-	return np.array(allcenters);
+		allareas.append(area)
+		cv2.drawContours(im, topcontours, -1, (255,0,0))
+		#cv2.imshow('sample1',im)
+		#cv2.waitKey()
+		cv2.imwrite(vidname+'/detect/'+frame,im)
+	return np.array(allcenters), np.array(allareas);
 
-find_centers('68clip')
-#create_frames('GOPR0076')
+if len(sys.argv) < 2:
+	print "USAGE: bkgdsubtract.py <clipname> [function 0/1]";
+
+vidname = sys.argv[1]
+
+if len(sys.argv) == 3:
+	if sys.argv[2] == 0:
+		create_frames(vidname)
+	else:
+		find_centers(vidname)
+else:
+	create_frames(vidname)
+	find_centers(vidname)
+
+#find_centers('68clip')
